585f7eea7ae2d95cdf42786edb0bcab9313f0791
fiorinishalom <fiorini.shalom@gmail.com>
2024-03-11 14:45:56 -0400

    .BadBM #1 made actual UI interface

--- lib/src/main/java/edu/touro/mco152/bm/UIWorker.java
+++ lib/src/main/java/edu/touro/mco152/bm/UIWorker.java
@@ -3,30 +3,24 @@ package edu.touro.mco152.bm;
 import java.beans.PropertyChangeListener;
 
 public interface UIWorker<V>  {
+    // creates a new thread to execute on.
+    void start();
+
     // Method to set the progress
     void updateProgress(int progress);
 
     // Method for publishing intermediate results
     void displayResults(V results);
 
+    // Method to cancel the background task
+    void makeCanceled(boolean bool);
 
-//   T doInBackground() throws Exception;
-//
-//    // Method to perform actions when the background task is done
-//    void done();
-//
-//
-//
-//    // Method to cancel the background task
-//    boolean cancel(boolean bool);
-//
-//    // Method to check if the background task is cancelled
-//    boolean isCancelled();
-//
-//    // creates a new thread to execute on.
-//    void execute();
-//
-//    void addPropertyChangeListener(PropertyChangeListener listener);
+    // Method to check if the background task is cancelled
+    boolean checkCancel();
+
+    void addPropertyChangeListener(PropertyChangeListener listener);
+
+
 
 
 }
