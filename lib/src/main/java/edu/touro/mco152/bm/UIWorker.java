b35b9c84a4eabbf81260b6b8e4e2de083ce40498
fiorinishalom <fiorini.shalom@gmail.com>
2024-03-10 21:45:46 -0400

    created interface to be implemented by any UI manager
    . Fixed #1

---
+++ lib/src/main/java/edu/touro/mco152/bm/UIWorker.java
@@ -0,0 +1,32 @@
+package edu.touro.mco152.bm;
+
+import java.beans.PropertyChangeListener;
+
+public interface UIWorker<V>  {
+    // Method to set the progress
+    void setProgress(int progress);
+
+    // Method for publishing intermediate results
+    void displayResults(V results);
+
+
+//   T doInBackground() throws Exception;
+//
+//    // Method to perform actions when the background task is done
+//    void done();
+//
+//
+//
+//    // Method to cancel the background task
+//    boolean cancel(boolean bool);
+//
+//    // Method to check if the background task is cancelled
+//    boolean isCancelled();
+//
+//    // creates a new thread to execute on.
+//    void execute();
+//
+//    void addPropertyChangeListener(PropertyChangeListener listener);
+
+
+}
