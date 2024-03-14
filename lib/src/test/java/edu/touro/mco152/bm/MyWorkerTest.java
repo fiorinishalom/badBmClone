e9c6e5ce7377f9c7b31bd8615eeffda9b8595642
fiorinishalom <fiorini.shalom@gmail.com>
2024-03-13 23:43:07 -0400

    Tried to make tests but i can get My benchmarking to start

---
+++ lib/src/test/java/edu/touro/mco152/bm/MyWorkerTest.java
@@ -0,0 +1,97 @@
+package edu.touro.mco152.bm;
+
+import edu.touro.mco152.bm.ui.Gui;
+import edu.touro.mco152.bm.ui.MainFrame;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyWorkerTest {
+    MyWorker myWorker;
+
+    @BeforeEach
+    void setUp(){
+        setupDefaultAsPerProperties();
+
+        DiskWorker worker;
+        worker = App.worker;
+
+        ;
+
+    }
+
+    @Test
+    void start() {
+        assertFalse(App.worker.uiWorker().getLastStatus());
+
+        try {
+            myWorker.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertTrue(myWorker.getLastStatus());
+
+        }
+
+    @Test
+    void doInBackground() {
+
+    }
+
+    @Test
+    void updateProgress() {
+
+    }
+
+    @Test
+    void getLastStatus() {
+
+    }
+    /**
+     * Bruteforce setup of static classes/fields to allow DiskWorker to run.
+     *
+     * @author lcmcohen
+     */
+    public static void setupDefaultAsPerProperties()
+    {
+        /// Do the minimum of what  App.init() would do to allow to run.
+        Gui.mainFrame = new MainFrame();
+        App.p = new Properties();
+        App.loadConfig();
+
+        Gui.progressBar = Gui.mainFrame.getProgressBar(); //must be set or get Nullptr
+
+        // configure the embedded DB in .jDiskMark
+        System.setProperty("derby.system.home", App.APP_CACHE_DIR);
+
+        // code from startBenchmark
+        //4. create data dir reference
+
+        // may be null when tests not run in original proj dir, so use a default area
+        if (App.locationDir == null) {
+            App.locationDir = new File(System.getProperty("user.home"));
+        }
+
+        App.dataDir = new File(App.locationDir.getAbsolutePath()+File.separator+App.DATADIRNAME);
+
+        //5. remove existing test data if exist
+        if (App.dataDir.exists()) {
+            if (App.dataDir.delete()) {
+                App.msg("removed existing data dir");
+            } else {
+                App.msg("unable to remove existing data dir");
+            }
+        }
+        else
+        {
+            App.dataDir.mkdirs(); // create data dir if not already present
+        }
+    }
+
+}
